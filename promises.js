e93017b07599bde737fe36e7e6ec40512e5f0ce2
Paul Frazee <pfrazee@gmail.com>
2012-12-27 10:29:04 -0600

    added tests; added environment boilerplate

--- promises.js
+++ promises.js
@@ -1,116 +1,109 @@
-function Promise(value) {
-	this.fulfillCBs = [];
-	this.exceptCBs = [];
-	this.value = undefined;
-	if (value) {
-		if (value instanceof Error) {
-			this.break(value);
-		} else {
-			this.fulfill(value);
-		}
-	}
+// promises
+// ========
+// pfraze 2012
+
+var environment = {};
+if (typeof window !== "undefined") {
+	environment = window;
+}else if (typeof module !== "undefined") {
+	environment = module.exports;
 }
-Promise.prototype.isUnfulfilled = function() { return (typeof this.value == 'undefined'); };
-Promise.prototype.isBroken = function() { return (this.value instanceof Error); };
-Promise.prototype.isFulfilled = function() { return (!this.isUnfulfilled() && !this.isBroken()); };
-function doThen(p, fn, args) {
-	try {
-		var value = fn.apply(p, [this.value].concat(args));
-		if (typeof value != 'undefined') {
-			p.fulfill(value);
-		}
-	}
-	catch (e) {
-		if (!(e instanceof Error)) { e = new Error(e); }
-		p.break(e);
-	}
-}
-Promise.prototype.then = function(fn) {
-	if (this.isBroken()) {
-		log('promise then noop');
-		return this;
-	} else {
-		var p = promise();
-		var args = Array.prototype.slice.call(arguments, 1);
-		if (this.isUnfulfilled()) {
-			log('promise then async');
-			this.fulfillCBs.push({ p:p, fn:fn, args:args }); // run on fulfill
-		} else {
-			log('promise then sync');
-			doThen.call(this, p, fn, args);
-		}
-		return p;
-	}
-};
-Promise.prototype.except = function(fn) {
-	if (this.isFulfilled()) {
-		log('promise except noop');
-		return this;
-	} else {
-		var p = promise();
-		var args = Array.prototype.slice.call(arguments, 1);
-		if (this.isUnfulfilled()) {
-			log('promise except async');
-			this.exceptCBs.push({ p:p, fn:fn, args:args }); // run on break
-		} else {
-			log('promise except sync');
-			doThen.call(this, p, fn, args);
-			if (p.isUnfulfilled()) {
-				p.break(this.value);
+
+(function (exports) {
+	function passThrough(v) { return v; }
+
+	function Promise(value) {
+		this.fulfillCBs = [];
+		this.exceptCBs = [];
+		this.value = undefined;
+		if (value) {
+			if (value instanceof Error) {
+				this.reject(value);
+			} else {
+				this.fulfill(value);
 			}
 		}
-		return p;
 	}
-};
-Promise.prototype.fulfill = function(value) {
-	if (this.isUnfulfilled()) {
-		log('promise fulfill');
-		this.value = value;
-		for (var i=0; i < this.fulfillCBs.length; i++) {
-			var cb = this.fulfillCBs[i];
-			doThen.call(this, cb.p, cb.fn, cb.args);
+	Promise.prototype.isUnfulfilled = function() { return (typeof this.value == 'undefined'); };
+	Promise.prototype.isBroken = function() { return (this.value instanceof Error); };
+	Promise.prototype.isFulfilled = function() { return (!this.isUnfulfilled() && !this.isBroken()); };
+	function doThen(p, fn, args) {
+		try {
+			var value = fn.apply(p, [this.value].concat(args));
+			if (typeof value != 'undefined') {
+				p.fulfill(value);
+			}
 		}
-		this.fulfillCBs.length = 0;
-		this.exceptCBs.length = 0;
+		catch (e) {
+			var err = e;
+			if (!(err instanceof Error)) { err = new Error(e); }
+			p.reject(err);
+		}
 	}
-};
-Promise.prototype['break'] = function(err) {
-	if (this.isUnfulfilled()) {
-		log('promise break');
-		if (!(err instanceof Error)) {
-			err = new Error(err);
+	Promise.prototype.then = function(fn) {
+		if (this.isBroken()) {
+			return this;
+		} else {
+			var p = promise();
+			var args = Array.prototype.slice.call(arguments, 1);
+			if (this.isUnfulfilled()) {
+				this.fulfillCBs.push({ p:p, fn:fn, args:args }); // run on fulfill
+				this.exceptCBs.push({ p:p, fn:passThrough, args:[] });
+			} else {
+				doThen.call(this, p, fn, args);
+			}
+			return p;
 		}
-		this.value = err;
-		for (var i=0; i < this.exceptCBs.length; i++) {
-			var cb = this.exceptCBs[i];
-			doThen.call(this, cb.p, cb.fn, cb.args);
+	};
+	Promise.prototype.except = function(fn) {
+		if (this.isFulfilled()) {
+			return this;
+		} else {
+			var p = promise();
+			var args = Array.prototype.slice.call(arguments, 1);
+			if (this.isUnfulfilled()) {
+				this.exceptCBs.push({ p:p, fn:fn, args:args }); // run on break
+				this.fulfillCBs.push({ p:p, fn:passThrough, args:[] });
+			} else {
+				doThen.call(this, p, fn, args);
+			}
+			return p;
 		}
-		this.fulfillCBs.length = 0;
-		this.exceptCBs.length = 0;
-	}
-};
-function promise(v) { return new Promise(v); }
+	};
+	Promise.prototype.fulfill = function(value) {
+		if (this.isUnfulfilled()) {
+			this.value = value;
+			for (var i=0; i < this.fulfillCBs.length; i++) {
+				var cb = this.fulfillCBs[i];
+				doThen.call(this, cb.p, cb.fn, cb.args);
+			}
+			this.fulfillCBs.length = 0;
+			this.exceptCBs.length = 0;
+		}
+	};
+	Promise.prototype.reject = function(err) {
+		if (this.isUnfulfilled()) {
+			if (!(err instanceof Error)) {
+				err = new Error(err);
+			}
+			this.value = err;
+			for (var i=0; i < this.exceptCBs.length; i++) {
+				var cb = this.exceptCBs[i];
+				doThen.call(this, cb.p, cb.fn, cb.args);
+			}
+			this.fulfillCBs.length = 0;
+			this.exceptCBs.length = 0;
+		}
+	};
+	function promise(v) { return (v instanceof Promise) ? v : new Promise(v); }
 
+	exports.Promise = Promise;
+	exports.promise = promise;
 
-function add(a, b) { log(a+'+'+b); return a + b; }
-function subtract(a, b) { log(a+'-'+b); return a - b; }
-function divide(a, b) { log(a+'/'+b); if (b === 0) { throw "Divide by zero"; } return a / b; }
+})(environment);
 
-var logging = true;
-function log(v) { if (logging) { output('LOG '+v); } };
-function output(v) { document.write(v + '<br/>'); return v; }
-function reportError(v) { document.write('error: ' + v + '<br/>'); }
-
-log('test four');
-promise(5).then(add, 5).then(subtract, 10).then(output); // => 0
-log('test five');
-promise(5).except(reportError).then(divide, 0).except(reportError).then(add, 5).then(output).except(reportError); // => error: Divide by zero
-
-function wait(value, time) {
-  var self = this;
-  setTimeout(function() { self.fulfill(value); }, time);
+if (typeof define !== "undefined") {
+	define([], function() {
+		return environment;
+	});
 }
-log('test six');
-
-//promise('asdf').then(wait, 10).then(output);
-//promise(5).then(wait, 5000).then(subtract, 10).then(output); // => [after five seconds] 0
---
+++ test/test.js
@@ -0,0 +1,116 @@
+var promise = require('../promises').promise;
+var Promise = require('../promises').Promise;
+var assert = require('assert');
+
+describe('promise', function() {
+	describe('then() sequence', function() {
+		it('should run fulfilled syncronous functions in order', function(done) {
+			promise(1).then(function(v) {
+				assert(v == 1);
+				return 2;
+			})
+			.then(function(v) {
+				assert(v == 2);
+				return 3;
+			})
+			.except(function(err) {
+				assert(false); // should never run
+			})
+			.then(function(v) {
+				assert(v == 3);
+				done();
+			});
+		});
+		it('should delay execution of sequence until fulfillment', function(done) {
+			var p = promise();
+			var a = 0;
+			p.then(function(v) {
+				assert(v == 1);
+				return 2;
+			})
+			.then(function(v) {
+				assert(v == 2);
+				return 3;
+			})
+			.except(function(err) {
+				assert(false); // should never run
+			})
+			.then(function(v) {
+				assert(v == 3);
+				a = 1;
+				done();
+			});
+			assert(a === 0);
+			p.fulfill(1);
+		});
+		it('should delay execution of an intermediary step until fulfillment', function(done) {
+			promise(1).then(function(v) {
+				assert(v == 1);
+				return 2;
+			})
+			.then(function(v) {
+				assert(v == 2);
+				var self = this;
+				setTimeout(function() { self.fulfill(3); }, 1);
+			})
+			.except(function(err) {
+				assert(false); // should never run
+			})
+			.then(function(v) {
+				assert(v == 3);
+				done();
+			});
+		});
+	});
+	describe('except() handling', function() {
+		it('should switch to except functions on error', function(done) {
+			promise(1).then(function(v) {
+				assert(v == 1);
+				return 2;
+			})
+			.then(function(v) {
+				throw "oh nooo";
+			})
+			.except(function(err) {
+				assert(err.message == "oh nooo");
+				return err;
+			})
+			.then(function(v) {
+				assert(false); // should never run
+			})
+			.except(function() {
+				done();
+			});
+		});
+		it('should switch to except functions on error with async', function(done) {
+			var p = promise();
+			p.then(function(v) {
+				assert(v == 1);
+				return 2;
+			})
+			.then(function(v) {
+				throw "oh nooo";
+			})
+			.except(function(err) {
+				assert(err.message == "oh nooo");
+				return err;
+			})
+			.then(function(v) {
+				assert(false); // should never run
+			})
+			.except(function() {
+				done();
+			});
+			p.fulfill(1);
+		});
+	});
+	describe('the promise() constructor', function() {
+		it('should create a promise out of a non-promise', function(done) {
+			promise('foobar').then(function(v) { assert(v == 'foobar'); done(); });
+		});
+		it('should return the input when it is a promise', function(done) {
+			var p = promise('foobar');
+			promise(p).then(function(v) { assert(v == 'foobar'); done(); });
+		});
+	});
+});
